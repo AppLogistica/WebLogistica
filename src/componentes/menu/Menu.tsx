d16a3d647cab5f750e7b5e21dd3d8639b177b841
AppLogistica <AppLogisticaSuporte@gmail.com>
2024-07-05 20:54:22 -0300

    Commit Final - Espero

--- src/componentes/menu/Menu.tsx
+++ src/componentes/menu/Menu.tsx
@@ -1,12 +1,8 @@
 export function Menu() {
 
     return (
-        <div className="menu" style={{ marginBottom: "2rem", backgroundColor: "#242424", display: "flex", justifyContent: "space-evenly", margin: "0 0 60px 0"}}>
-            <a href="/main"
-                style={{
-                    textDecoration: "none",
-                    textAlign: "center"
-                }}>
+        <div className="menu" style={{ marginBottom: "2rem", backgroundColor: "#242424", display: "flex", justifyContent: "space-evenly", margin: "0 0 60px 0" }}>
+            <a href="/main" style={{ textDecoration: "none", textAlign: "center" }}>
                 <div
                     style={{
                         width: "200%",
@@ -15,83 +11,64 @@ export function Menu() {
                         justifyContent: "center",
                         alignItems: "center",
                         borderRadius: "8px",
-                        backgroundColor: '#8e00a8', 
-                        color: 'white',  
+                        backgroundColor: '#8e00a8',
+                        color: 'white',
                         border: '2px solid #FFFFFF',
-                    }}>Inicio</div></a>
+                    }}>
+                    Inicio
+                </div>
+            </a>
 
-            <a href="/email"
-                style={{
-                    textDecoration: "none",
-                    textAlign: "center"
-                }}>
+            <a href="/email" style={{ textDecoration: "none", textAlign: "center" }}>
                 <div
                     style={{
-                        backgroundColor: 'white',
                         width: "200%",
                         height: "140%",
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
                         borderRadius: "8px",
-                        backgroundColor: '#8e00a8', 
-                        color: 'white',  
+                        backgroundColor: '#8e00a8', // Cor corrigida
+                        color: 'white',
                         border: '2px solid #FFFFFF',
-                    }}>Email</div> </a>
+                    }}>
+                    Email
+                </div>
+            </a>
 
-            <a href="/fornecedores"
-                style={{
-                    textDecoration: "none",
-                    textAlign: "center"
-                }}>
-                <div style={{
-                    backgroundColor: 'white',
-                    width: "140%",
-                    height: "140%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: "8px",
-                    backgroundColor: '#8e00a8', 
-                    color: 'white',  
-                    border: '2px solid #FFFFFF',
-                }}>Fornecedores</div> </a>
+            <a href="/fornecedores" style={{ textDecoration: "none", textAlign: "center" }}>
+                <div
+                    style={{
+                        width: "140%",
+                        height: "140%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: "8px",
+                        backgroundColor: '#8e00a8', // Cor corrigida
+                        color: 'white',
+                        border: '2px solid #FFFFFF',
+                    }}>
+                    Fornecedores
+                </div>
+            </a>
 
-            <a href="/caixa"
-                style={{
-                    textDecoration: "none",
-                    textAlign: "center"
-                }}>
-                <div style={{
-                    backgroundColor: 'white',
-                    width: "200%",
-                    height: "140%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: "8px",
-                    backgroundColor: '#8e00a8', 
-                    color: 'white',  
-                    border: '2px solid #FFFFFF',
-                }}>Caixa</div> </a>
-
+            <a href="/caixa" style={{ textDecoration: "none", textAlign: "center" }}>
+                <div
+                    style={{
+                        width: "200%",
+                        height: "140%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: "8px",
+                        backgroundColor: '#8e00a8', // Cor corrigida
+                        color: 'white',
+                        border: '2px solid #FFFFFF',
+                    }}>
+                    Caixa
+                </div>
+            </a>
         </div>
-
     )
 }
-
-/*
-<a href="/local"
-style={{
-    textDecoration: "none",
-    textAlign: "center"
-}}>
-<div style={{
-    backgroundColor: 'white',
-    width: "140%",
-    height: "140%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: "8px"
-}}>Local</div> </a>*/
